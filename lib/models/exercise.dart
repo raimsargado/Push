87649fde05d9034332d2bfb58937f6fe855a2ccb
jxxxefrxxm <mac@macs-MacBook-Pro.local>
2019-09-18 23:00:44 +0800

    logic for adding exercise

---
+++ lib/models/exercise.dart
@@ -0,0 +1,5 @@
+class Exercise {
+  final String name;
+
+  Exercise(this.name);
+}
---
+++ lib/models/workset.dart
@@ -0,0 +1,17 @@
+
+
+class WorkSet {
+  final String id;
+  final String previous;
+  final String weight;
+  final String reps;
+  final String tag;
+
+  WorkSet(
+    this.id,
+    this.previous,
+    this.weight,
+    this.reps,
+    this.tag,
+  );
+}
